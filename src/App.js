02b55f1087d25a100eccbd93242a2cd79771cbb1
BC\s2100646 <Deepika.MaliniRajasekar@edu.bc.fi>
2022-05-06 14:31:04 +0300

    Add navBar component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,58 @@ import './App.css';
 
 
 
-function App() {
-  return (
+class App extends Component {
+  state = { 
+        counters : [
+             {id:1 , value:0},
+             {id:2 , value:0},
+             {id:3 , value:0},
+             {id:4 , value:0},
+             {id:5 , value:0},
+             {id:6 , value:0},
+        ] 
+     };
+     handleDelete = (counterId) => {
+         console.log("Event handler delete called!!!" , counterId); 
+         const counters = this.state.counters.filter(c => c.id !== counterId); // we cannot dírectly modify the object in react so creat new array and filter the counter which has the id not equals  
+         //this.setState({counters: counters}) overright counters object with counters const but key and value are same so one counters is enough
+         this.setState({counters});
+     };
+     handleReset = () => {
+         const counters =  this.state.counters.map( c => 
+            {
+                c.value = 0;
+                return c;
+            })
+            this.setState({counters});
+     };
+
+     handleIncrement = counter  =>{
+        const counters = [...this.state.counters]; // cloning counters array from state object to counters const
+
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter}; // cloning counter object to  the array element corresponding to the index
+        counters[index].value ++ ;
+        this.setState({counters});
+     }
+  render() { 
+    return (
     <React.Fragment>
      <NavBar/>
      <main className="container">
-     <Counters/>
+     <Counters
+     counters = {this.state.counters} 
+     onReset = {this.handleReset} 
+     onIncrement ={this.handleIncrement}
+     onDelete = {this.handleDelete} />
      </main>
     </React.Fragment>
-  );
+    );
+  }
 }
+
+  
+  
+
 
 export default App;
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    // state = { 
-    //     count : this.props.counter.value
-    //  } 
-     
-    //  handleIncrement = () => {
-    //      console.log("Increment clicked" , this);
-    //      this.setState( {count: this.state.count + 1});
-    //  };
-
-    render() { 
-        
+    render() {        
         return (
             <div>
                 <span className= {this.getBadgeClasses()}> {this.formatCount()} </span>
                 <button  onClick={() => this.props.onIncrement(this.props.counter)}className='btn btn-secondary btn-sm'>Increment</button>
                 <button className='btn btn-danger btn-sm m-2' onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
-
             </div>
         )
     }
